6fd439608c4c51b0da7878f6d127735111028f1e
ChandRun <chandrakant0203@gmail.com>
2023-01-10 23:25:17 +0530

    First Java App

---
+++ src/main/java/jfsd/corejava/Main.java
@@ -0,0 +1,8 @@
+package jfsd.corejava;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Call from main class");
+    }
+}
